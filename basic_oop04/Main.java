d089b710a07034330df8590a6459f1850ac6670e
phitly <phitly@outlook.com>
2025-09-30 21:48:27 -0400

    notes on using mul classes

---
+++ basic_oop04/Main.java
@@ -0,0 +1,16 @@
+/// 
+///Using Multiple Classes
+
+///You can also create an object of a class
+///and access it in another class. 
+/// This is often used for better organization of classes
+/// (one class has all the attributes and methods, while 
+/// the other class holds the main() method (code to be executed)). 
+
+
+public class Main {
+    int x =5;
+    String main = "Hello World";    
+
+    
+}
---
+++ basic_oop04/Second.java
@@ -0,0 +1,9 @@
+public class Second {
+    public static void main(String[] args) {
+        Main myObject = new Main();
+        System.out.println(myObject.x);
+        System.out.println(myObject.main);
+    }
+}
+/// this is the second file with the main method
+/// it runs the code in the Main class
